b9e82dcde7cc4d9b2555ea359bfe16d26e18763b
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-04-12 11:35:39 +0000

    feat: update service name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'app.postica.baked',
-  appName: 'Baked',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
-};
+    appId: 'app.postica.bakingnews',
+    appName: 'Baking News',
+    webDir: 'dist',
+    server: {
+        androidScheme: 'https',
+    },
+}
 
-export default config;
+export default config
