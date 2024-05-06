f39e862018100e85cef9b1055414126bbbcfdf49
rycont <rycont>
2024-05-06 06:14:36 +0000

    chore: add shade-ui submodule

--- extFetch.ts
+++ extFetch.ts
@@ -1,4 +1,3 @@
-import { Capacitor } from '@capacitor/core'
 import { cfetch } from './utils/cfetch'
 
-export const extFetch = Capacitor.getPlatform() === 'web' ? cfetch : fetch
+export const extFetch = cfetch
